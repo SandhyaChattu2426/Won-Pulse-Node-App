99800030f8e4bcec7e89b1cc12a4e77551e62b9f
SandhyaChattu2426 <sandhya.chattu@nowitservices.com>
2025-04-07 12:59:09 +0530

    msg

--- mongoServer/models/staff.js
+++ mongoServer/models/staff.js
@@ -23,11 +23,11 @@ const staffSchema = new Schema({
         password: { type: String, },
         role: { type: String },
         doctorType: { type: String, required: false },
-        is_mfa_enabled: { type: String ,default: "false" } ,
+        is_mfa_enabled: { type: String, default: "false" },
         mfa_type: { type: [String], default: [] },
         passkey: { type: String },
         registeredOn: { type: Date, default: Date.now },
-        // add emergencyContactdetails As per the requirement
-},{ minimize: true })
+        DoctorAppointments: [{ type: String, required: false }]
+}, { minimize: true })
 
 module.exports = mongoose.model('Staff', staffSchema)
