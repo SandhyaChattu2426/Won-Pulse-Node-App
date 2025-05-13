6eabefb1d9cab9cfd52ac6da5437206ed1159854
SandhyaChattu2426 <sandhya.chattu@nowitservices.com>
2025-05-13 15:55:16 +0530

    Message

--- mongoServer/routes/PBillRequests.js
+++ mongoServer/routes/PBillRequests.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const RequestControllers=require("../controllers/pbrControllers")
-router.post('/',RequestControllers.CreateRequest)
-router.get('/',RequestControllers.GetNotifiedBills)
-router.get('/:id/hospital/:hospitalId',RequestControllers.GetBillRequestById)
+const RequestControllers = require("../controllers/pbrControllers")
+router.post('/', RequestControllers.CreateRequest)
+router.get('/', RequestControllers.GetNotifiedBills)
+router.get('/:id/hospital/:hospitalId', RequestControllers.GetBillRequestById)
 // router.patch('/:id/status/hospital/:hospitalId',RequestControllers.UpdateStatus)
 
 module.exports = router
